266e5048e1d589f861f67628810e749e07508662
storylight5555-gif <storylight5555@gmail.com>
2026-08-09 13:25:11 +0400

    homework8

--- Homework/homework8.py
+++ Homework/homework8.py
@@ -1,21 +1,8 @@
-def calculate_power():
-    print("--- Vrisha's Power Calculator ---")
-    
-    try:
-        base = float(input("Enter the base number: "))
-        exponent = float(input("Enter the power (n): "))
-        
-        result = base ** exponent
-        
-        
-        if base.is_integer(): base = int(base)
-        if exponent.is_integer(): exponent = int(exponent)
-        if result.is_integer(): result = int(result)
-            
-        print(f"\nResult: {base} raised to the power of {exponent} is {result}")
-        
-    except ValueError:
-        print("Error: Please enter valid numbers only.")
+base= int(input("Enter the base number: "))
+n= int(input("Enter the exponent number: "))
 
-if __name__ == "__main__":
-    calculate_power()
+result = 1
+for exponent in range(1, n + 1):
+    result=1
+    result = result * base
+    print(f"{base} ^ {exponent} = {result}")
